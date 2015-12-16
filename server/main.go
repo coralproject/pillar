866b67bf328b2084e00af9d4109053e094ad3149
Gabriela Rodriguez <gabelula@gmail.com>
2015-12-16 13:06:44 -0800

    Adds install file.

--- server/main.go
+++ server/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"github.com/coralproject/pillar/server/web"
 	"log"
 	"net/http"
+
+	"github.com/coralproject/pillar/server/web"
 )
 
 func main() {
 
 	router := web.NewRouter()
-
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
