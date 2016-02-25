67479504de6f66555868c9db435524979e107983
Eric Buth <ericdotbuth@gmail.com>
2016-02-25 15:01:10 -0500

    backend interface and mongodb implementation

---
+++ pkg/backend/backend.go
@@ -0,0 +1,18 @@
+package backend
+
+import (
+	"errors"
+	"github.com/coralproject/pillar/pkg/backend/iterator"
+)
+
+type Query map[interface{}]interface{}
+
+var (
+	BackendNotInitializedError = errors.New("backend not initialized")
+	BackendTypeError           = errors.New("object type is incorrect")
+)
+
+type Backend interface {
+	Find(objectType string, query map[string]interface{}) (iterator.Iterator, error)
+	Close() error
+}
