32849d04ecbf4ceb47cf53745c18527fe6e1eafa
Eric Buth <ericdotbuth@gmail.com>
2016-02-25 16:50:40 -0500

    user statistics accumulator

--- pkg/model/statistics/user.go
+++ pkg/model/statistics/user.go
@@ -1,6 +1,8 @@
 package statistics
 
 import (
+	"golang.org/x/net/context"
+
 	"github.com/coralproject/pillar/pkg/model"
 )
 
@@ -14,6 +16,24 @@ type UserComments CommentDimensions
 type UserStatistics struct {
 	Actions  *UserActions  `json:"actions" bson:"actions"`
 	Comments *UserComments `json:"comments" bson:"comments"`
+}
+
+type UserStatisticsAccumulator struct {
+	Comments *CommentDimensionsAccumulator
+}
+
+func NewUserStatisticsAccumulator() *UserStatisticsAccumulator {
+	return &UserStatisticsAccumulator{
+		Comments: NewCommentDimensionsAccumulator(),
+	}
+}
+
+func (a *UserStatisticsAccumulator) Accumulate(ctx context.Context, object interface{}) {
+	a.Comments.Accumulate(ctx, object)
+}
+
+func (a *UserStatisticsAccumulator) Combine(object interface{}) {
+	a.Comments.Combine(object)
 }
 
 type User struct {
