8bc501a77f717e93830b888e1c5292505f782884
samshub <samaresh@gmail.com>
2016-03-08 15:27:27 -0500

    keeping the code for using gorilla context, just in case.

--- app/pillar/handler/mware.go
+++ app/pillar/handler/mware.go
@@ -2,6 +2,10 @@ package handler
 
 import (
 	"github.com/rs/cors"
+	"github.com/codegangsta/negroni"
+	"net/http"
+	"github.com/coralproject/pillar/pkg/service"
+	"github.com/gorilla/context"
 )
 
 const appContext string = "app-context"
@@ -15,38 +19,35 @@ func CORS() *cors.Cors {
 	})
 }
 
-//func AppHandler() negroni.Handler {
-//	return negroni.HandlerFunc(func(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-//
-//		// set header
-//		rw.Header().Set("Content-Type", "application/json")
-//
-//		//Create/Inject DB
-//		c := service.NewContext()
-//		defer c.Close()
-//
-//		//Create/Inject AppContext for the Handlers to use
-//		SetAppContext(r, c)
-//		next(rw, r)
-//	})
-//}
-//
-//func GetAppContext(r *http.Request, input interface{}) *service.AppContext {
-//
-//	rc := context.Get(r, appContext)
-//	if rc == nil {
-//		return nil
-//	}
-//
-//	c := rc.(*service.AppContext)
-//	//inject input data if any
-//	if input != nil {
-//		c.Input = input
-//	}
-//
-//	return c
-//}
-//
-//func SetAppContext(r *http.Request, val *service.AppContext) {
-//	context.Set(r, appContext, val)
-//}
+func AppHandler() negroni.Handler {
+	return negroni.HandlerFunc(func(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+
+		// set header
+		rw.Header().Set("Content-Type", "application/json")
+
+		//Create/Inject DB
+		c := service.NewContext(nil)
+		defer c.Close()
+
+		//Create/Inject AppContext for the Handlers to use
+		SetAppContext(r, c)
+		next(rw, r)
+	})
+}
+
+func GetAppContext(r *http.Request, input interface{}) *service.AppContext {
+
+	rc := context.Get(r, appContext)
+	if rc == nil {
+		return nil
+	}
+
+	//inject input data if any
+	c := rc.(*service.AppContext)
+	c.Marshall(input)
+	return c
+}
+
+func SetAppContext(r *http.Request, val *service.AppContext) {
+	context.Set(r, appContext, val)
+}
