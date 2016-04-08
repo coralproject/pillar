a318b462c7a36bcb72d2ef806a93bc9d3ecb8cef
samshub <samaresh@gmail.com>
2016-04-08 15:57:03 -0400

    Changed filters from arrary to bson.M.

--- pkg/model/search.go
+++ pkg/model/search.go
@@ -10,9 +10,9 @@ type Search struct {
 	ID          bson.ObjectId `json:"id" bson:"_id"`
 	Name        string        `json:"name" bson:"name" validate:"required"`
 	Description string        `json:"description,omitempty" bson:"description,omitempty"`
-	Query    	string        `json:"query" bson:"query" validate:"required"`
-	Tag     	string        `json:"tag" bson:"tag" validate:"required"`
-	Filters     []interface{} `json:"filters,omitempty" bson:"filters,omitempty"`
+	Query       string        `json:"query" bson:"query" validate:"required"`
+	Tag         string        `json:"tag" bson:"tag" validate:"required"`
+	Filters     bson.M        `json:"filters,omitempty" bson:"filters,omitempty"`
 	Results     []interface{} `json:"results,omitempty" bson:"results,omitempty"`
 	DateCreated time.Time     `json:"date_created" bson:"date_created" validate:"required"`
 	DateUpdated time.Time     `json:"date_updated,omitempty" bson:"date_updated,omitempty"`
@@ -21,8 +21,8 @@ type Search struct {
 }
 
 type SearchHistory struct {
-	ID          bson.ObjectId `json:"id" bson:"_id"`
-	Action      string        `json:"action" bson:"action" validate:"required"`
-	Date        time.Time     `json:"date" bson:"date" validate:"required"`
-	Search      Search        `json:"search" bson:"search" validate:"required"`
+	ID     bson.ObjectId `json:"id" bson:"_id"`
+	Action string        `json:"action" bson:"action" validate:"required"`
+	Date   time.Time     `json:"date" bson:"date" validate:"required"`
+	Search Search        `json:"search" bson:"search" validate:"required"`
 }
