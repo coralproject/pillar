4d8700f18ea7093dfa5435e31dabb8453ed56921
samshub <samaresh@gmail.com>
2016-05-21 09:36:00 -0400

    moved init() to cron to print xenia env.

--- pkg/service/search_cron.go
+++ pkg/service/search_cron.go
@@ -6,7 +6,13 @@ import (
 	"github.com/coralproject/pillar/pkg/web"
 	"gopkg.in/mgo.v2/bson"
 	"log"
+	"os"
 )
+
+func init() {
+	log.Printf("Xenia URL: %s\n", os.Getenv("XENIA_URL"))
+	log.Printf("Xenia Auth: %s\n", os.Getenv("XENIA_AUTH"))
+}
 
 func UpdateSearch() {
 
@@ -110,4 +116,3 @@ func getNewUsers(db *db.MongoDB, search model.Search) (map[bson.ObjectId]model.U
 
 	return m, a
 }
-
