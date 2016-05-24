655d29dd532497cf8978249caad8fc5b63b9cbc5
samshub <samaresh@gmail.com>
2016-05-24 16:00:13 -0400

    Added new query-param for xenia - this will come from env.

--- pkg/service/xenia.go
+++ pkg/service/xenia.go
@@ -27,7 +27,7 @@ type doc struct {
 }
 
 func getUserIds(search model.Search) []string {
-	url := os.Getenv("XENIA_URL") + search.Query
+	url := os.Getenv("XENIA_URL") + search.Query + os.Getenv("XENIA_QUERY_PARAM")
 
 	header := make(map[string]string)
 	header["Content-Type"] = "application/json"
